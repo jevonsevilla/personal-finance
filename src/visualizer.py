3c1dd783712b01b3e8d05cec09892e1c92851ec9
Jevon Sevilla <jevonangtuacosevilla@gmail.com>
2025-01-03 01:01:22 +0800

    add unimplemented decorator

--- src/visualizer.py
+++ src/visualizer.py
@@ -1,8 +1,8 @@
 import plotly.express as px
 
 
-class Visualizer:
-    """class to visualize results."""
+class Plots:
+    """class to plot results."""
 
     @staticmethod
     def plot_net_worth(cashflow) -> None:
@@ -17,3 +17,26 @@ class Visualizer:
             hovermode="x unified",  # Ensures that hover information for the same x is shown for both traces
         )
         plt.show()
+
+
+class Print:
+    """class to print text."""
+
+    @staticmethod
+    def print_dict(title: str, stats: dict) -> None:
+        max_key_width = max(len(key) for key in stats.keys())
+
+        print(f"{title}:")
+        for key, (value) in stats.items():
+            try:
+                print(f"{key:{max_key_width}} : {value:{15},.2f}")
+            except ValueError:
+                print(f"{key:{max_key_width}} : {str(value):>{15}}")
+
+    # @staticmethod
+    # def print_arguments(func):
+    #     def wrapper(*args, **kwargs):
+    #         print(f"Calling {func.__name__}")
+    #         Print.print_dict("Func Name", kwargs)
+
+    #     return wrapper
